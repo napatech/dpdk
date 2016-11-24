c45c41adb9ca43ae973c6db7f35f510f58b1635d
Qiming Yang <qiming.yang@intel.com>
2016-11-24 15:56:18 +0800

    net/i40e: fixed build error with icc
    Fixes: 718d2475357a ("40e: fix VF bonded device link down")
    
    Signed-off-by: Qiming Yang <qiming.yang@intel.com>

--- drivers/net/i40e/i40e_pf.c
+++ drivers/net/i40e/i40e_pf.c
@@ -906,7 +906,7 @@ i40e_notify_vf_link_status(struct rte_eth_dev *dev, struct i40e_pf_vf *vf)
 	event.event_data.link_event.link_status =
 			dev->data->dev_link.link_status;
 	event.event_data.link_event.link_speed =
-			dev->data->dev_link.link_speed;
+			(enum i40e_aq_link_speed)dev->data->dev_link.link_speed;
 	i40e_pf_host_send_msg_to_vf(vf, I40E_VIRTCHNL_OP_EVENT,
 			I40E_SUCCESS, (uint8_t *)&event, sizeof(event));
 }
