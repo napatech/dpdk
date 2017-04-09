603f75e6ca1592bf6cb642f940e9a1be80f2b533
Qi Zhang <qi.z.zhang@intel.com>
2017-04-09 14:05:06 -0400

    net/i40e: add missing 25G link speed
    [ upstream commit 8a4764f8b7f986b9e30b91f6f56606b82605c00e ]
    
    Add missing 25G link speed decode in i40evf_dev_link_update.
    
    Fixes: 75d133dd3296 ("net/i40e: enable 25G device")
    
    Signed-off-by: Qi Zhang <qi.z.zhang@intel.com>

--- drivers/net/i40e/i40e_ethdev_vf.c
+++ drivers/net/i40e/i40e_ethdev_vf.c
@@ -2146,6 +2146,9 @@ i40evf_dev_link_update(struct rte_eth_dev *dev,
 	case I40E_LINK_SPEED_20GB:
 		new_link.link_speed = ETH_SPEED_NUM_20G;
 		break;
+	case I40E_LINK_SPEED_25GB:
+		new_link.link_speed = ETH_SPEED_NUM_25G;
+		break;
 	case I40E_LINK_SPEED_40GB:
 		new_link.link_speed = ETH_SPEED_NUM_40G;
 		break;
