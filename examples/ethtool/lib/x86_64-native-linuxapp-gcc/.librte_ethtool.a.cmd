7720f208fb5c3685c6a3fef9eeb553aee2b513df
Bent Kuhre <bk@napatech.com>
2017-08-14 18:11:26 +0200

    Removed examples build files

--- examples/ethtool/lib/x86_64-native-linuxapp-gcc/.librte_ethtool.a.cmd
+++
@@ -1,1 +0,0 @@
-cmd_librte_ethtool.a = ar crDs librte_ethtool.a rte_ethtool.o 
--- examples/performance-thread/pthread_shim/x86_64-native-linuxapp-gcc/.ctx.o.d
+++
@@ -1,4 +0,0 @@
-dep_ctx.o = \
- /usr/project/dpdk_napatech/examples/performance-thread/common/arch/x86/ctx.c \
- /usr/include/stdc-predef.h \
- /usr/project/dpdk_napatech//x86_64-native-linuxapp-gcc/include/rte_config.h
