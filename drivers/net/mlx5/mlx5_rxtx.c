0a3e50ffc2ffcf91360813c58fc3b3b42531125d
Shahaf Shuler <shahafs@mellanox.com>
2017-09-14 13:50:38 +0300

    net/mlx5: fix Tx stats error counter logic
    [ upstream commit 24c14430cdc4556a30a1e608f67230e881718f7f ]
    
    Tx error counter lacks the logic of incrementation, making it useless
    for applications.
    
    Fixes: 87011737b715 ("mlx5: add software counters")
    
    Signed-off-by: Shahaf Shuler <shahafs@mellanox.com>
    Acked-by: Yongseok Koh <yskoh@mellanox.com>
    Acked-by: Nelio Laranjeiro <nelio.laranjeiro@6wind.com>

--- drivers/net/mlx5/mlx5_rxtx.c
+++ drivers/net/mlx5/mlx5_rxtx.c
@@ -431,8 +431,10 @@ mlx5_tx_burst(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 #ifdef MLX5_PMD_SOFT_COUNTERS
 		total_length = length;
 #endif
-		if (length < (MLX5_WQE_DWORD_SIZE + 2))
+		if (length < (MLX5_WQE_DWORD_SIZE + 2)) {
+			txq->stats.oerrors++;
 			break;
+		}
 		/* Update element. */
 		(*txq->elts)[elts_head] = buf;
 		elts_head = (elts_head + 1) & (elts_n - 1);
@@ -735,8 +737,10 @@ mlx5_tx_burst_mpw(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		if (max < segs_n + 1)
 			break;
 		/* Do not bother with large packets MPW cannot handle. */
-		if (segs_n > MLX5_MPW_DSEG_MAX)
+		if (segs_n > MLX5_MPW_DSEG_MAX) {
+			txq->stats.oerrors++;
 			break;
+		}
 		max -= segs_n;
 		--pkts_n;
 		/* Should we enable HW CKSUM offload */
@@ -941,8 +945,10 @@ mlx5_tx_burst_mpw_inline(void *dpdk_txq, struct rte_mbuf **pkts,
 		if (max < segs_n + 1)
 			break;
 		/* Do not bother with large packets MPW cannot handle. */
-		if (segs_n > MLX5_MPW_DSEG_MAX)
+		if (segs_n > MLX5_MPW_DSEG_MAX) {
+			txq->stats.oerrors++;
 			break;
+		}
 		max -= segs_n;
 		--pkts_n;
 		/* Should we enable HW CKSUM offload */
