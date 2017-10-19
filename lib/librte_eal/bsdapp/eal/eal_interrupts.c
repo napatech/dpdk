4250d8298911eb71f0dd0e1e11b25ce5913ea1c5
Bruce Richardson <bruce.richardson@intel.com>
2017-10-19 17:36:26 +0100

    eal/bsd: fix missing interrupt stub functions
    [ upstream commit 3a207c8ff4a8b56a49001e0d1957150298b24794 ]
    
    A number of interrupt functions only existed on Linux. Adding in stubs
    for these functions corrects this omission, and allows the map files for
    both Linux and FreeBSD to be identical.
    
    Fixes: 9efe9c6cdcac ("eal/linux: add epoll wrappers")
    
    Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>

--- lib/librte_eal/bsdapp/eal/eal_interrupts.c
+++ lib/librte_eal/bsdapp/eal/eal_interrupts.c
@@ -117,3 +117,32 @@ rte_intr_cap_multiple(struct rte_intr_handle *intr_handle)
 	RTE_SET_USED(intr_handle);
 	return 0;
 }
+
+int
+rte_epoll_wait(int epfd, struct rte_epoll_event *events,
+		int maxevents, int timeout)
+{
+	RTE_SET_USED(epfd);
+	RTE_SET_USED(events);
+	RTE_SET_USED(maxevents);
+	RTE_SET_USED(timeout);
+
+	return -ENOTSUP;
+}
+
+int
+rte_epoll_ctl(int epfd, int op, int fd, struct rte_epoll_event *event)
+{
+	RTE_SET_USED(epfd);
+	RTE_SET_USED(op);
+	RTE_SET_USED(fd);
+	RTE_SET_USED(event);
+
+	return -ENOTSUP;
+}
+
+int
+rte_intr_tls_epfd(void)
+{
+	return -ENOTSUP;
+}
