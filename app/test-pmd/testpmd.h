2c5ee7ede57471ce59e9f1eb0aa02e2e3ba0062e
Matan Azrad <matan@mellanox.com>
2018-05-03 10:31:46 +0000

    app/testpmd: fix synchronic port hotplug
    [ upstream commit 03ce2c53a3905b11e54c424ca395ae5bea186cbd ]
    
    When the user uses the synchronic hot-plug commands, attach\detach, in
    order to insert\remove a port from the system, the forward ports list
    update is missed in the current implementation.
    
    Thus, an invalid port may be used for data-path in case of detach
    because the detached port was not removed from the forward port list.
    
    In addition, a new port is not used for data-path in case of attach, as the
    default behavior of Testpmd, because the attached port was not inserted
    to the forward port list.
    
    Update the forward port list in the above cases to allow the correct
    port usage for data-path in the next packet forwarding start.
    
    Fixes: edab33b1c01d ("app/testpmd: support port hotplug")
    
    Signed-off-by: Matan Azrad <matan@mellanox.com>

--- app/test-pmd/testpmd.h
+++ app/test-pmd/testpmd.h
@@ -599,6 +599,7 @@ void fwd_config_setup(void);
 void set_def_fwd_config(void);
 void reconfig(portid_t new_port_id, unsigned socket_id);
 int init_fwd_streams(void);
+void update_fwd_ports(portid_t new_pid);
 
 void port_mtu_set(portid_t port_id, uint16_t mtu);
 void port_reg_bit_display(portid_t port_id, uint32_t reg_off, uint8_t bit_pos);
