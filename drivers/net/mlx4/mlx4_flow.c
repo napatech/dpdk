d0024f989c3f39c648dc10ae4f7fcd87d1e5937a
Adrien Mazarguil <adrien.mazarguil@6wind.com>
2018-06-04 17:40:27 -0700

    net/mlx4: fix UDP flow rule limitation enforcement
    [ upstream commit bf959ec58b5d9127f187e98de6d2d0976c111360 ]
    
    For some unknown reason, priorities do not have any effect on flow rules
    that happen to match UDP destination ports. Those are always matched first
    regardless.
    
    This patch is a workaround that enforces this limitation at the PMD level;
    such flow rules can only be created at the highest priority level for
    correctness.
    
    Fixes: a5171594fc3b ("net/mlx4: expose support for flow rule priorities")
    Cc: stable@dpdk.org
    
    Signed-off-by: Adrien Mazarguil <adrien.mazarguil@6wind.com>

--- drivers/net/mlx4/mlx4_flow.c
+++ drivers/net/mlx4/mlx4_flow.c
@@ -379,6 +379,9 @@ error:
  * Additional mlx4-specific constraints on supported fields:
  *
  * - No support for partial masks.
+ * - Due to HW/FW limitation, flow rule priority is not taken into account
+ *   when matching UDP destination ports, doing is therefore only supported
+ *   at the highest priority level (0).
  *
  * @param[in, out] flow
  *   Flow rule handle to update.
@@ -408,6 +411,11 @@ mlx4_flow_merge_udp(struct rte_flow *flow,
 	    ((uint16_t)(mask->hdr.src_port + 1) > UINT16_C(1) ||
 	     (uint16_t)(mask->hdr.dst_port + 1) > UINT16_C(1))) {
 		msg = "mlx4 does not support matching partial UDP fields";
+		goto error;
+	}
+	if (mask && mask->hdr.dst_port && flow->priority) {
+		msg = "combining UDP destination port matching with a nonzero"
+			" priority level is not supported";
 		goto error;
 	}
 	if (!flow->ibv_attr)
@@ -674,6 +682,7 @@ mlx4_flow_prepare(struct priv *priv,
 			 NULL, "only ingress is supported");
 fill:
 	proc = mlx4_flow_proc_item_list;
+	flow->priority = attr->priority;
 	/* Go over pattern. */
 	for (item = pattern; item->type; ++item) {
 		const struct mlx4_flow_proc_item *next = NULL;
