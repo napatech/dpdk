0356e485f46e3a365ac3f3d26dab7ca6d62c0fc0
Xueming Li <xuemingl@mellanox.com>
2018-06-04 17:35:09 -0700

    net/mlx5: map UAR address around huge pages
    [ backported from upstream commit 4a984153430cbdafe2b971e999bba8d96610b299 ]
    
    Reserving the memory space for the UAR near huge pages helps to
    **reduce** the cases where the secondary process cannot start. Those
    pages being physical pages they must be mapped at the same virtual
    address as in the primary process to have a
    working secondary process.
    
    As this remap is almost the latest being done by the processes
    (libraries, heaps, stacks are already loaded), similar to huge pages,
    there is **no guarantee** this mechanism will always work.
    
    Signed-off-by: Xueming Li <xuemingl@mellanox.com>
    Acked-by: Nelio Laranjeiro <nelio.laranjeiro@6wind.com>

--- drivers/net/mlx5/mlx5.h
+++ drivers/net/mlx5/mlx5.h
@@ -166,6 +166,7 @@ struct priv {
 	struct mlx5_xstats_ctrl xstats_ctrl; /* Extended stats control. */
 	rte_spinlock_t lock; /* Lock for control functions. */
 	int primary_socket; /* Unix socket for primary process. */
+	void *uar_base; /* Reserved address space for UAR mapping */
 	struct rte_intr_handle intr_handle_socket; /* Interrupt handler. */
 	struct mlx5_verbs_alloc_ctx verbs_alloc_ctx;
 	/* Context for Verbs allocator. */
