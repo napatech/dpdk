db46654737739acf858c3087fda9ccfcbd288da9
Rafal Kozik <rk@semihalf.com>
2018-07-27 12:39:05 +0200

    net/ena: check pointer before memset
    [ backported from upstream commit 46916aa17d4b2007df8c0454f99ba0ca8b8cb93b ]
    
    Need to check if memory allocation succeed before using it.
    Using memset on NULL pointer cause segfault.
    
    Fixes: 9ba7981ec992 ("ena: add communication layer for DPDK")
    
    Signed-off-by: Rafal Kozik <rk@semihalf.com>
    Acked-by: Michal Krawczyk <mk@semihalf.com>

--- drivers/net/ena/base/ena_plat_dpdk.h
+++ drivers/net/ena/base/ena_plat_dpdk.h
@@ -191,10 +191,15 @@ typedef uint64_t dma_addr_t;
 		snprintf(z_name, sizeof(z_name),			\
 				"ena_alloc_%d", ena_alloc_cnt++);	\
 		mz = rte_memzone_reserve(z_name, size, SOCKET_ID_ANY, 0); \
-		memset(mz->addr, 0, size);				\
-		virt = mz->addr;					\
-		phys = mz->iova;					\
 		handle = mz;						\
+		if (mz == NULL) {					\
+			virt = NULL;					\
+			phys = 0;					\
+		} else {						\
+			memset(mz->addr, 0, size);			\
+			virt = mz->addr;				\
+			phys = mz->iova;				\
+		}							\
 	} while (0)
 #define ENA_MEM_FREE_COHERENT(dmadev, size, virt, phys, handle) 	\
 		({ ENA_TOUCH(size); ENA_TOUCH(phys);			\
@@ -209,9 +214,14 @@ typedef uint64_t dma_addr_t;
 		snprintf(z_name, sizeof(z_name),			\
 				"ena_alloc_%d", ena_alloc_cnt++);	\
 		mz = rte_memzone_reserve(z_name, size, node, 0); \
-		memset(mz->addr, 0, size);				\
-		virt = mz->addr;					\
-		phys = mz->iova;					\
+		if (mz == NULL) {					\
+			virt = NULL;					\
+			phys = 0;					\
+		} else {						\
+			memset(mz->addr, 0, size);			\
+			virt = mz->addr;				\
+			phys = mz->iova;				\
+		}							\
 	} while (0)
 
 #define ENA_MEM_ALLOC_NODE(dmadev, size, virt, node, dev_node) \
