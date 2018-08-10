53843cccde3e3be204d716f93b0f07e26482bcac
Bent Kuhre <bk@napatech.com>
2018-08-10 11:06:11 +0200

    NT version updated

--- lib/librte_eal/common/include/rte_version.h
+++ lib/librte_eal/common/include/rte_version.h
@@ -59,7 +59,7 @@ extern "C" {
 /**
 * Extra Napatech minor version numbers
 */
-#define RTE_NT_MINOR_VER 8
+#define RTE_NT_MINOR_VER 9
 
 /**
 * Extra Napatech string to be appended to version number
