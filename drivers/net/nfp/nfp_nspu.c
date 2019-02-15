117d12452b30eccca4d05d4f83b64dd1f1b0acad
Alejandro Lucero <alejandro.lucero@netronome.com>
2019-02-15 10:30:44 +0000

    net/nfp: fix misuse of strlcpy
    Current strlcpy function is doing the wrong thing and as a consequence
    the firmware does not find the symbol requested precluding the right
    NFP initialization.
    
    Using strncpy is safe here since the symbol length can never be longer
    than the buffer size where the firmware will get the symbol to work
    with. However, newer compilers do not allow to have the strncpy using
    the source length as the third parameter, so this patch uses instead a
    memcpy call with a previous memset for cleaning up the buffer to be
    used by the firmware.
    
    Fixes: a5d659c2d03f ("net/nfp: replace strncpy by strlcpy")
    
    Signed-off-by: Alejandro Lucero <alejandro.lucero@netronome.com>

--- drivers/net/nfp/nfp_nspu.c
+++ drivers/net/nfp/nfp_nspu.c
@@ -424,7 +424,9 @@ nfp_nspu_set_bar_from_symbl(nspu_desc_t *desc, const char *symbl,
 	if (!sym_buf)
 		return -ENOMEM;
 
-	strlcpy(sym_buf, symbl, sizeof(sym_buf));
+	memset(sym_buf, 0, desc->buf_size);
+	memcpy(sym_buf, symbl, strlen(symbl));
+
 	ret = nspu_command(desc, NSP_CMD_GET_SYMBOL, 1, 1, sym_buf,
 			   NFP_SYM_DESC_LEN, strlen(symbl));
 	if (ret) {
