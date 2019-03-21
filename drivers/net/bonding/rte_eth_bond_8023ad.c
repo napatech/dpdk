ca538e25f0c02f47a39949f7dc41c8411483b914
Hui Zhao <zhaohui8@huawei.com>
2019-03-21 21:28:13 +0100

    net/bonding: fix slave id types
    [ upstream commit c28aff1e41eedd9d44c480264efbd7f4dd5cf31e ]
    
    mode_bond_id and mode_band_id are slave ids, stored on 16bits.
    
    Fixes: f8244c6399d9 ("ethdev: increase port id range")
    
    Signed-off-by: Hui Zhao <zhaohui8@huawei.com>
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>

--- drivers/net/bonding/rte_eth_bond_8023ad.c
+++ drivers/net/bonding/rte_eth_bond_8023ad.c
@@ -638,7 +638,7 @@ tx_machine(struct bond_dev_private *internals, uint16_t slave_id)
 	SM_FLAG_CLR(port, NTT);
 }
 
-static uint8_t
+static uint16_t
 max_index(uint64_t *a, int n)
 {
 	if (n <= 0)
@@ -672,7 +672,8 @@ selection_logic(struct bond_dev_private *internals, uint16_t slave_id)
 	uint64_t agg_bandwidth[8] = {0};
 	uint64_t agg_count[8] = {0};
 	uint16_t default_slave = 0;
-	uint8_t mode_count_id, mode_band_id;
+	uint16_t mode_count_id;
+	uint16_t mode_band_id;
 	struct rte_eth_link link_info;
 
 	slaves = internals->active_slaves;
@@ -708,13 +709,11 @@ selection_logic(struct bond_dev_private *internals, uint16_t slave_id)
 
 	switch (internals->mode4.agg_selection) {
 	case AGG_COUNT:
-		mode_count_id = max_index(
-				(uint64_t *)agg_count, slaves_count);
+		mode_count_id = max_index(agg_count, slaves_count);
 		new_agg_id = mode_count_id;
 		break;
 	case AGG_BANDWIDTH:
-		mode_band_id = max_index(
-				(uint64_t *)agg_bandwidth, slaves_count);
+		mode_band_id = max_index(agg_bandwidth, slaves_count);
 		new_agg_id = mode_band_id;
 		break;
 	case AGG_STABLE:
