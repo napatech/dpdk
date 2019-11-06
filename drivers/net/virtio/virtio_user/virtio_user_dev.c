71ae3517754aa162257988e6c34746ffc20d78aa
Marvin Liu <yong.liu@intel.com>
2019-11-06 17:02:50 +0800

    net/virtio-user: fix setting filters
    [ upstream commit a76552d48f17a990662592ff5e13a6e83b62025c ]
    
    As doc mentioned, Rx/Mac/vlan filters are all supported by best effort.
    These control commands should return success.
    
    Fixes: f9b9d1a55775 ("net/virtio-user: add multiple queues in device emulation")
    
    Signed-off-by: Marvin Liu <yong.liu@intel.com>
    Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>

--- drivers/net/virtio/virtio_user/virtio_user_dev.c
+++ drivers/net/virtio/virtio_user/virtio_user_dev.c
@@ -600,6 +600,10 @@ virtio_user_handle_ctrl_msg(struct virtio_user_dev *dev, struct vring *vring,
 
 		queues = *(uint16_t *)(uintptr_t)vring->desc[idx_data].addr;
 		status = virtio_user_handle_mq(dev, queues);
+	} else if (hdr->class == VIRTIO_NET_CTRL_RX  ||
+		   hdr->class == VIRTIO_NET_CTRL_MAC ||
+		   hdr->class == VIRTIO_NET_CTRL_VLAN) {
+		status = 0;
 	}
 
 	/* Update status */
