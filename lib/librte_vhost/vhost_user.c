afc8c11865ef4770b4b19bb6610fa497767530a0
Zhike Wang <wangzk320@163.com>
2019-11-14 16:16:29 +0100

    vhost: fix vring requests validation broken if no FD
    [ upstream commit 1407b0752eee2353160701d0182c7e911ccb538a]
    
    When VHOST_USER_VRING_NOFD_MASK is set, the fd_num is 0,
    so validate_msg_fds() will return error. In this case,
    the negotiation of vring message between vhost user front end and
    back end would fail, and as a result, vhost user link could NOT be up.
    
    How to reproduce:
    1.Run dpdk testpmd insides VM, which locates at host with ovs+dpdk.
    2.Notice that inside ovs there are endless logs regarding failure to
    handle VHOST_USER_SET_VRING_CALL, and link of vm could NOT be up.
    
    Fixes: f8898927bb16 ("vhost: fix possible denial of service by leaking FDs")
    
    Signed-off-by: Zhike Wang <wangzk320@163.com>
    Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Reviewed-by: Tiwei Bie <tiwei.bie@intel.com>

--- lib/librte_vhost/vhost_user.c
+++ lib/librte_vhost/vhost_user.c
@@ -1241,8 +1241,10 @@ vhost_user_set_vring_call(struct virtio_net **pdev, struct VhostUserMsg *msg,
 	struct virtio_net *dev = *pdev;
 	struct vhost_vring_file file;
 	struct vhost_virtqueue *vq;
+	int expected_fds;
 
-	if (validate_msg_fds(msg, 1) != 0)
+	expected_fds = (msg->payload.u64 & VHOST_USER_VRING_NOFD_MASK) ? 0 : 1;
+	if (validate_msg_fds(msg, expected_fds) != 0)
 		return VH_RESULT_ERR;
 
 	file.index = msg->payload.u64 & VHOST_USER_VRING_IDX_MASK;
@@ -1266,7 +1268,10 @@ static int vhost_user_set_vring_err(struct virtio_net **pdev __rte_unused,
 			struct VhostUserMsg *msg,
 			int main_fd __rte_unused)
 {
-	if (validate_msg_fds(msg, 1) != 0)
+	int expected_fds;
+
+	expected_fds = (msg->payload.u64 & VHOST_USER_VRING_NOFD_MASK) ? 0 : 1;
+	if (validate_msg_fds(msg, expected_fds) != 0)
 		return VH_RESULT_ERR;
 
 	if (!(msg->payload.u64 & VHOST_USER_VRING_NOFD_MASK))
@@ -1283,8 +1288,10 @@ vhost_user_set_vring_kick(struct virtio_net **pdev, struct VhostUserMsg *msg,
 	struct virtio_net *dev = *pdev;
 	struct vhost_vring_file file;
 	struct vhost_virtqueue *vq;
+	int expected_fds;
 
-	if (validate_msg_fds(msg, 1) != 0)
+	expected_fds = (msg->payload.u64 & VHOST_USER_VRING_NOFD_MASK) ? 0 : 1;
+	if (validate_msg_fds(msg, expected_fds) != 0)
 		return VH_RESULT_ERR;
 
 	file.index = msg->payload.u64 & VHOST_USER_VRING_IDX_MASK;
